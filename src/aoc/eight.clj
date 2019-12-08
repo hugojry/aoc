bd9b166ea1d61652746cba1b7b81465bcc44eb57
Hugo Young <hybas3@gmail.com>
2019-12-08 23:02:29 +0000

    Day eight

---
+++ src/aoc/eight.clj
@@ -0,0 +1,36 @@
+(ns aoc.eight
+  (:require [clojure.string :as string]))
+
+(defn string->digits
+  [image]
+  (map (comp read-string str) image))
+
+(defn color
+  [pixels]
+  (case (first pixels)
+    0 :black
+    1 :white
+    2 (recur (rest pixels))))
+
+(defn color->char
+  [c]
+  (if (= c :white) \0 \space))
+
+(comment 
+  
+  (def image (string/trim (slurp "eight.txt")))
+
+  (def digits (string->digits image))
+
+  (def colors (partition 25 (->> (partition (* 25 6) digits)
+                                 (apply map list)
+                                 (map color))))
+
+  (loop [rows colors]
+    (if-some [row (first rows)]
+      (do
+        (println (apply str (map color->char row)))
+        (recur (rest rows)))
+      nil))
+  
+  )
