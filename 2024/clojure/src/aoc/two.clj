260593267193c9efa9e85ea932108cb83006622b
Hugo Young <hybas3@gmail.com>
2024-12-03 12:17:50 +0000

    day 2

---
+++ 2024/clojure/src/aoc/two.clj
@@ -0,0 +1,25 @@
+(ns aoc.two
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn part-1-pred [report]
+  (let [differences (map #(apply - %) (partition 2 1 report))]
+    (and (or (every? neg? differences)
+             (every? pos? differences))
+         (< (apply max (map abs differences)) 4))))
+
+(defn part-2-pred [report]
+  (or (part-1-pred report)
+      (let [v (vec report)]
+        (some part-1-pred (for [i (range (count v))]
+                            (into (subvec v 0 i) (subvec v (inc i))))))))
+
+(comment
+
+  (def reports (->> (io/resource "two.txt")
+                    io/reader
+                    line-seq
+                    (map #(map parse-long (string/split % #" ")))))
+
+  (count (filter part-1-pred reports))
+  (count (filter part-2-pred reports)))
